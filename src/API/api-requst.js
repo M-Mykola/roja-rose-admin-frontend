f68b007920eb9cde4894b6e4a2fbb15595104355
Mykola Matiko <matiko.mikola@gmail.com>
2022-10-31 16:19:48 +0200

    Rename branch.

--- src/API/api-requst.js
+++ src/API/api-requst.js
@@ -1,11 +1,11 @@
-import axios from "axios";
+// import axios from "axios";
 
-export const GetUserRequest = async () => {
-  return axios({
-    url: "",
-    headers: {
-      //   "x-access-token": token.value,
-    },
-    method: "GET",
-  });
-};
+// export const GetUserRequest = async () => {
+//   return axios({
+//     url: "",
+//     headers: {
+//       //   "x-access-token": token.value,
+//     },
+//     method: "GET",
+//   });
+// };
